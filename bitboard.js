d568c301e937a8ea97abf7e505a133df2e4b185c
Eric Martindale <eric@ericmartindale.com>
2016-06-24 15:51:26 -0700

    initial scratchup of a single Resource definition

--- bitboard.js
+++ bitboard.js
@@ -2,4 +2,12 @@ var config = require('./config');
 var Maki = require('maki');
 var bitboard = new Maki(config);
 
+var Job = bitboard.define('job', {
+  icon: 'suitcase',
+  description: 'This is a job posting on the Bitboard.',
+  attributes: {
+    title: { type: String , max: 140 , required: true }
+  }
+});
+
 bitboard.start();
