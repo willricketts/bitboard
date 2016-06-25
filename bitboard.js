89d65bebf7079e5685111289f1856c7389fe2a9f
Will Ricketts <will@willricketts.com>
2016-06-24 20:37:48 -0400

    cleanup

--- bitboard.js
+++ bitboard.js
@@ -7,6 +7,7 @@ var userResource = require('./resources/User');
 var categoryResource = require('./resources/Category');
 var jobResource = require('./resources/Job');
 
+// Resource Definitions
 var User = bitboard.define('user', userResource);
 var Category = bitboard.define('category', categoryResource);
 var Job = bitboard.define('job', jobResource);
