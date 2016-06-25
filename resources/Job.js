54a9a38ef82c9a125e9f844e62dad13912bca49c
Will Ricketts <will@willricketts.com>
2016-06-24 21:28:11 -0400

    adding proper date handling

--- resources/Job.js
+++ resources/Job.js
@@ -1,5 +1,3 @@
-var moment = require('moment');
-
 var job = {
   icon: 'suitcase',
   description: 'This is a job posting on the Bitboard.',
@@ -7,7 +5,7 @@ var job = {
     title: { type: String , max: 140 , required: true },
     content: { type: String, max: 5000, min: 100, required: true },
     paid: { type: Boolean, required: true, default: false },
-    created: { type: Date, required: true, default: moment() }
+    created: { type: Date, required: true, default: Date.now }
   }
 }
 
